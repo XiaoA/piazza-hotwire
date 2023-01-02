3d61e0506fe0cf0178c866e15517d893f54eebfe
Andrew Buckingham <abuckingham99@gmail.com>
2023-01-01 20:46:05 -0800

    Define user name/email validations

---
+++ test/models/user_test.rb
@@ -0,0 +1,32 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  test "requires a name" do
+    @user = User.new(name: "", email: "andrew@example.com")
+
+    assert_not @user.valid?
+  end
+
+  test "requires a valid email" do
+    @user = User.new(name: "Andrew", email: "")
+    assert_not @user.valid?
+
+    @user.email = "invalid"
+    assert_not @user.valid?
+
+    @user.email = "andrew@example.com"
+    assert @user.valid?
+  end
+
+  test "requires a unique email" do
+    @existing_user = User.create(
+      name: "Andrew", email: "andrew@example.com"
+    )
+    assert @existing_user.persisted?
+
+    @user = User.new(
+      name: "Drew", email: "andrew@example.com"
+    )
+    assert_not @user.valid?
+  end
+end
