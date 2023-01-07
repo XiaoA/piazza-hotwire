8102fec70d60b1126b7df848f24754ce2fbab89a
Andrew Buckingham <abuckingham99@gmail.com>
2023-01-06 20:28:04 -0800

    Users and registrations (#9)
    * Define user name/email validations
    
    * Set up user relationships
    
    * Strip whitespace from 'name' and 'email' form fields
    
    * Add 'minitest-reporters' gem
    
    * Set up user authentication
    
    * Set up app title helper
    
    * Create Feed controller
    
    * Add Users controller
    
    * Update routes
    
    * Set up locale files
    
    * Fix typo on user sign_up page
    
    * Set up sign up form with flash welcome message
    
    * Implement form error messages
    
    * Add 'i18n-debug' gem
    
    * Complete sign-up form
    
    * Add `password_confirmation` field to sign in form

--- Gemfile
+++ Gemfile
@@ -37,7 +37,7 @@ gem "redis", "~> 4.0"
 # gem "kredis"
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
-# gem "bcrypt", "~> 3.1.7"
+gem "bcrypt", "~> 3.1.7"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
@@ -56,7 +56,7 @@ end
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
-
+  gem "i18n-debug"
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
 
@@ -69,4 +69,5 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
   gem "webdrivers"
+  gem 'minitest-reporters', '~> 1.5'
 end
---
+++ test/models/user_test.rb
@@ -0,0 +1,77 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  test "requires a name" do
+    @user = User.new(
+      name: "",
+      email: "andrew@example.com",
+      password: "password",
+      password: "password",
+    )
+
+    assert_not @user.valid?
+  end
+
+  test "requires a valid email" do
+    @user = User.new(
+      name: "Andrew",
+      email: "",
+      password: "password",
+      password_confirmation: "password"
+    )
+    assert_not @user.valid?
+
+    @user.email = "invalid"
+    assert_not @user.valid?
+
+    @user.email = "andrew@example.com"
+    assert @user.valid?
+  end
+
+  test "requires a unique email" do
+    @existing_user = User.create(
+      name: "Andrew",
+      email: "andrew@example.com",
+      password: "password",
+      password_confirmation: "password",
+    )
+    assert @existing_user.persisted?
+
+    @user = User.new(
+      name: "Drew",
+      email: "andrew@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+    assert_not @user.valid?
+  end
+
+  test "name and email fields do not contain whitespaces" do
+    @user = User.create(
+      name: " Andrew ",
+      email: " andrew@example.com ",
+      password: "password",
+      password_confirmation: "password",
+    )
+
+    assert_equal "Andrew", @user.name
+    assert_equal "andrew@example.com", @user.email
+  end
+
+  test "password length must be between 8 and ActiveModel's maximum" do
+    @user = User.new(
+      name: "Sherlock",
+      email: "sherlock@example.com",
+      password: "",
+      password_confirmation: "",
+    )
+    assert_not @user.valid?
+
+    @user.password = @user.password_confirmation = "password"
+    assert @user.valid?
+
+    max_length = ActiveModel::SecurePassword::MAX_PASSWORD_LENGTH_ALLOWED
+    @user.password = @user.password_confirmation = "a" * (max_length + 1)
+    assert_not @user.valid?
+  end
+end
