b82d74e9340566ba8ec5d6e91e5423b5324e2ec0
Andrew Buckingham <abuckingham99@gmail.com>
2023-01-08 09:40:16 -0800

    Refactor user authentication

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -9,7 +9,6 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     assert_difference("@user.app_sessions.count", 1) {
       log_in(@user)
     }
-    
     assert_not_empty cookies[:app_session]
     assert_redirected_to root_path
   end
